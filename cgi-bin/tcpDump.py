47058f82a668325b9f3df110a91c2c850ff85e84
m206762 <m206762@usna.edu>
2020-02-05 10:42:11 -0500

    try2

--- cgi-bin/tcpDump.py
+++ cgi-bin/tcpDump.py
@@ -2,13 +2,30 @@ import subprocess
 import re
 
 def tokenizeOutput(text):
-    rgxStr='([\d]*\:[\d]*\:[\d]*\.[\d]*) IP ([\d]*\.[\d]*.[\d]*\.[\d]*)\.(\w*|\d*) \> ([\d]*\.[\d]*.[\d]*\.[\d]*\.)(\w*|\d*): Flags \[([SAFRPU.])*\],( seq \d*:\d*,)*( ack \d*,)*( win \d*,)*( options \[((\w*)|(\w*,))*.*])*, length \d*'
+    rgxStr='[\d]*\:[\d]*\:[\d]*\.[\d]* IP ([\d]*\.[\d]*.[\d]*\.[\d]*)\.(\w*|\d*) \> ([\d]*\.[\d]*.[\d]*\.[\d]*\.)(\w*|\d*)'
     x = re.search(rgxStr,text)
-    print(x)
+    if x = None:
+        return 1
+    hostIP=x[0]
+    hostService=x[1]
+    targetIP=x[2]
+    targetService=x[3]
+    if hostIP in outputDict:
+        if hostService in outputDict[hostIP]:
+            outputDict[hostIP][hostService] += 1
+        else:
+            outputDict[hostIP][hostService] = 1
+    else:
+        outputDict[hostIP]={hostService:1}
+    return 0
+
 try:
+    outputDict={}
     p = subprocess.Popen(('sudo', 'tcpdump', '-l'), stdout=subprocess.PIPE)
     for row in iter(p.stdout.readline, b''):
-        tokenizeOutput(str(row))
+        output = tokenizeOutput(str(row))
+        if output == 0:
+            print outputDict
 
 
 except KeyboardInterrupt:
